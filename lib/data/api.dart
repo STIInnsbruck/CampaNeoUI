709cb184d255081c249574a5eed7c3f9451806f6
Antonio J. Roa Valverde <914983+narko@users.noreply.github.com>
2020-07-10 22:41:38 +0200

    Feature/ui iteration (#9)
    * Added initial logic to control screen resizing in the Campaign details screen
    
    * Adding graphql faker
    
    * Cleaning unused code
    
    * Reorganizing graphql files
    
    * Reorganizing graphql files
    
    * Pointing graphql url to heroku

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
+// TODO handle this with config file
 const String URI_PROD = 'http://graphql.campaneo.vwn.cloud/graphql';
+const String URI_INT = 'https://campaneo.herokuapp.com/graphql';
 const String URI_DEV = 'http://localhost:9002/graphql';
 
 class ApiClient {
-  static final HttpLink httpLink = HttpLink(uri: URI_DEV);
+  static final HttpLink httpLink = HttpLink(uri: URI_INT);
 
   static ValueNotifier<GraphQLClient> initClient() => ValueNotifier(
         GraphQLClient(
